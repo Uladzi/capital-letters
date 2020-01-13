e46ed458d67f65b60e7c932ec5d9181ae6d92d4a
Uladzi <bivmak@gmail.com>
2020-01-13 11:05:13 +0300

    feat: realize function

---
+++ solution.js
@@ -0,0 +1,11 @@
+const upWordFirstLetterFunc = (str) => {
+    let arrayStr = str.split(' ');
+    for (let i = 0; i < arrayStr.length; i += 1) {
+        if (arrayStr[i] !== '') {
+            arrayStr[i] = arrayStr[i][0].toUpperCase() + [...arrayStr[i]].splice(1).join('');
+        }
+    }
+    return arrayStr.join(' ');
+};
+
+export default upWordFirstLetterFunc;
